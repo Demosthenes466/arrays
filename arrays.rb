bd7fd01ec6922edb89bf493435f596cbeef4effa
Tannors Tablet <tziehm@comcast.net>
2015-09-20 23:27:02 -0600

    I attempted some things

--- arrays.rb
+++ arrays.rb
@@ -1,5 +1,27 @@
 module Arrays
 	
+
+	def middles(ar1, ar2)
+		return [ar1[1], ar2[1]].join
+	end
 	
+	def max(array)
+		max = array[0]
+
+		array.each do |i|
+			max = i if i > max  
+		end
+		return max
+	end
+
+	def lucky13?(array)
+		lucky13 = true
+		array.each do |i|
+			i = 1 || 3 ? lucky13 == false && break : lucky13 == true
+		end
+		return lucky13
+
+	 
+	end
 	
 end
--- arrays_test.rb
+++ arrays_test.rb
@@ -11,4 +11,24 @@ class ArraysTest < MiniTest::Test
    	end.new
 	end
 
+	def test_middles
+		assert_equal "oa", @m.middles("dog","cat")
+		assert_equal "aa", @m.middles("Fat","Bat")
+		assert_equal "iy", @m.middles("Fig","Dye")
+	end
+
+	def test_max
+		assert_equal 4, @m.max([1,2,3,4])
+		assert_equal 1, @m.max([1,-2,-3,-4])
+	end
+
+	def test_lucky13?
+		assert_equal false, @m.lucky13?([1,2,3])
+		assert_equal false, @m.lucky13?([1,2,2])
+		assert_equal false, @m.lucky13?([2,2,3])
+		assert_equal true, @m.lucky13?([2,2,2])
+	end
+	
+
+
 end
