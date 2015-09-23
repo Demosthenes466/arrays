809ac0e452c144067d107e77e545b1bf3d7095a2
Tannors Tablet <tziehm@comcast.net>
2015-09-22 21:48:07 -0600

    I have fixed plus_it and written zero_front, but cant fix starry?

--- arrays.rb
+++ arrays.rb
@@ -17,11 +17,109 @@ module Arrays
 	def lucky13?(array)
 		lucky13 = true
 		array.each do |i|
-			i = 1 || 3 ? lucky13 == false && break : lucky13 == true
+			if ((i == 1) || (i == 3))
+				return lucky13 == false
+			end
 		end
 		return lucky13
+	end
 
-	 
+	def repeat_seperator(word, seperator, count)
+		array = ""
+
+		for i in [0..count.to_i] do
+			array += array + "#{word}""#{seperator}" * (count - 1) + word
+		end
+		return array
+	end
+
+	def same_ends?(array, ends)
+		if array.first(ends) == array.last(ends)
+			return true
+		else
+			return false
+		end
+	end
+
+	def starry?(str)
+		starry = false
+		array = str.split(//)
+
+		array.each do |i|
+			index = array.index("*")
+			puts "#{array[index-1]}"
+			puts "#{array[index+1]}"
+			if("#{array[(index-1)]}" == "#{array[(index+1)]}")
+				puts "This is running"
+				return starry == true
+			else
+				array.delete(index)
+			end
+		end
+	end
+		
+		# return index
+		
+
+		# starry = false
+		# sub = 0
+		# for i in [0..str.length] do
+		# 	pos = str.index("*")
+		# 	puts "#{pos}"
+		# 	puts "#{str[pos-1]}"
+		# 	puts "#{str[pos+1]}"
+		# 	if("#{str[pos+1]}" == "#{str[pos-1]}")
+		# 		return true
+		# 		# starry == true
+		# 	else
+		# 		str.slice(pos)
+		# 		# str == str.slice!(pos)
+		# 		puts "else"
+		# 		puts "#{str}"
+		# 	end
+		# end
+
+		# return false
+# end
+
+
+	def plus_it(word, token)
+		word_array = word.split(//)
+		token_array = token.split(//)
+		for j in (1..token.length-1) do
+			puts !word_array.include?(token_array[j])
+			if( !word_array.include?(token_array[j]) )
+				length = word_array.length
+				new_array = Array.new(length)
+				new_array.fill('+')
+				this = new_array.join
+				return this
+			end
+		end
+		for i in (0..word.length-token.length) do
+			if(word_array[i..(i+token.length)] != token_array)
+				puts"this"
+				word_array.delete_at(i)
+				word_array.insert(i, '+')
+				puts word_array
+			end
+		end
+		final = word_array.join
+		return final
+	end
+
+	def zero_front(array)
+		zero_array = []
+		for i in (0..array.length) do
+			index = array.find_index(0)
+			if (index == nil)
+				zero_array
+				final_array = zero_array + array
+				return final_array
+			end
+			array.delete_at(index)
+			zero_array[i] = 0
+		end
 	end
 	
 end
--- arrays_test.rb
+++ arrays_test.rb
@@ -29,6 +29,36 @@ class ArraysTest < MiniTest::Test
 		assert_equal true, @m.lucky13?([2,2,2])
 	end
 	
+	def test_repeat_seperator
+		assert_equal "ThisAndThis", @m.repeat_seperator("This", "And", 2)
+		assert_equal "ThisAndThisAndThis", @m.repeat_seperator("This", "And", 3)
+		assert_equal "This", @m.repeat_seperator("This", "And", 1)
+	end
+
+	def test_same_ends?
+		assert_equal true, @m.same_ends?([5, 6, 45, 99, 23, 5, 6], 2)
+		assert_equal false, @m.same_ends?([5, 6, 45, 99, 23, 5, 6], 3)
+		assert_equal true, @m.same_ends?([5, 6, 45, 99, 5, 6, 45], 3)
+	end
+
+	def test_starry?
+		assert_equal true, @m.starry?("a*a")
+		assert_equal false, @m.starry?("a*b")
+		assert_equal false, @m.starry?("a*a*b")
+	end
+
+	def test_plus_it
+		assert_equal "+at", @m.plus_it("cat", "at")
+		assert_equal "++at", @m.plus_it("that", "at")
+		assert_equal "+++", @m.plus_it("dog", "at")
+	end
+
+	def test_zero_front
+		assert_equal [0, 0, 5, 3, 1, 6], @m.zero_front([5, 0, 3, 1, 0, 6])
+		assert_equal [0, 5, 4, 3, 1, 6], @m.zero_front([5, 4, 3, 1, 0, 6])
+		assert_equal [0, 0, 0, 0, 0, 5], @m.zero_front([5, 0, 0, 0, 0, 0])
+		assert_equal [0, 0, 0, 0, 0, 0], @m.zero_front([0, 0, 0, 0, 0, 0])
+	end
 
 
 end
